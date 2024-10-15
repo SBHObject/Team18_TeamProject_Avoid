b8f9b760de6f4721352cb5d9a8132ab81e865753
SBHObject <168382040+SBHObject@users.noreply.github.com>
2024-10-15 19:11:54 +0900

    fix: Character move system change
    캐릭터 움직임 기능 변환- 가속도 추가

--- Assets/Scripts/Rtan.cs
+++ Assets/Scripts/Rtan.cs
@@ -4,47 +4,85 @@ using UnityEngine;
 
 public class Rtan : MonoBehaviour
 {
-    float direction = 0.05f;
-    SpriteRenderer renderer;
+    private float speed = 0.05f;
+    private SpriteRenderer spriteRenderer;
+
+    private float acceleration = 0.05f;
+    private float targetSpeed;
+    private float nowSpeed = 0;
+
+    [SerializeField]
+    private bool isHardMode;
 
     // Start is called before the first frame update
     void Start()
     {
-        Application.targetFrameRate = 60;
-        renderer = GetComponent<SpriteRenderer>();
+        Application.targetFrameRate = 60;   
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        targetSpeed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveDirection = 0f;
+        Move();
+    }
 
-        // 왼쪽 방향 이동
-        if (Input.GetKey(KeyCode.LeftArrow))
+    private void Move()
+    {
+        //방향 결정
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            moveDirection = -1f;
-            renderer.flipX = true;  // 왼쪽으로 향할 때 스프라이트를 뒤집음
+            LookLeft();
         }
 
-        // 오른쪽 방향 이동
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            moveDirection = 1f;
-            renderer.flipX = false;  // 오른쪽으로 향할 때 스프라이트를 정상 방향으로 설정
+            LookRight();
         }
 
-        // 이동
-        transform.position += Vector3.right * moveDirection * direction;
-
-        // 화면 경계 체크
-        if (transform.position.x > 2.6f)
+        //맵 탈출 방지
+        if(transform.position.x > 2.8f)
         {
-            transform.position = new Vector3(2.6f, transform.position.y, transform.position.z);
+            nowSpeed = 0;
+            LookLeft();
+            transform.position = new Vector3(2.8f, transform.position.y, 0);
         }
 
-        if (transform.position.x < -2.6f)
+        if (transform.position.x < -2.8f)
         {
-            transform.position = new Vector3(-2.6f, transform.position.y, transform.position.z);
+            nowSpeed = 0;
+            LookRight();
+            transform.position = new Vector3(-2.8f, transform.position.y, 0);
         }
+
+        //하드모드 여부에 따라 가속도 여부 결정
+        if (isHardMode)
+        {
+            nowSpeed = Mathf.Lerp(nowSpeed, targetSpeed, acceleration);
+            if(Mathf.Abs(nowSpeed) >= Mathf.Abs(targetSpeed) - 0.0001)
+            {
+                nowSpeed = targetSpeed;
+            }
+        }
+        else
+        {
+            nowSpeed = targetSpeed;
+        }
+
+        //실제 이동 구현부
+        transform.position += Vector3.right * nowSpeed;
+    }
+
+    private void LookLeft()
+    {
+        targetSpeed = speed * -1;
+        spriteRenderer.flipX = true;
+    }
+
+    private void LookRight()
+    {
+        targetSpeed = speed * 1f;
+        spriteRenderer.flipX = false;
     }
 }
