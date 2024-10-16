dd9cd3cb2467471ce31114c998a13b874c7f1e49
KimSungHo-web <epdlam20@gmail.com>
2024-10-16 12:44:53 +0900

    (OTH)feature_2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,10 +6,13 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
     public Transform spawnPoint;  // 캐릭터 스폰 위치
     public GameObject rain;
+    public GameObject bomb;
+    public GameObject shield;
     public GameObject endPanel;
     public Text totalScoreTxt;
 
     private int totalScore;
+    private float randomValue;
 
     private void Awake()
     {
@@ -42,8 +45,32 @@ public class GameManager : MonoBehaviour
             Debug.LogError("선택된 캐릭터 프리팹이 없습니다.");
         }
 
+<<<<<<< HEAD
         InvokeRepeating("MakeRain", 0f, 1f);  // 비 내리기 실행
+=======
+        InvokeRepeating("MakeRain", 0f, 0.5f);
+        InvokeRepeating("DropItem", 1f, 1f);
+>>>>>>> origin/OTH_feature
     }
+
+    void DropItem()
+    {
+        randomValue = Random.Range(0f, 1f);
+        if (randomValue <= 0.1f)
+        {
+            randomValue = Random.Range(0f, 1f);
+            if (randomValue <= 0.5f)
+            {
+                Instantiate(shield);
+            }
+            else
+            {
+                Instantiate(bomb);
+            }
+        }
+        
+    }
+
 
     void MakeRain()
     {
