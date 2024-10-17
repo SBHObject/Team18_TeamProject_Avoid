430b4757593085134e3157eeca55103b7c3d2cfa
PRODOINGER <wjddydghk668@naver.com>
2024-10-17 16:24:20 +0900

    10.17_1

--- Assets/Scripts/RetryButton.cs
+++ Assets/Scripts/RetryButton.cs
@@ -6,6 +6,9 @@ public class RetryButton : MonoBehaviour
     public GameObject singleOrMultiUI;
     public GameObject characterUI;
     public GameObject levelUI;
+    // 난이도 선택 및 씬 이동
+    // 정적 변수로 난이도 설정
+    public static float gameSpeed = 1.0f;
 
     // 싱글/멀티 UI 활성화
     public void ShowSingleOrMultiUI()
@@ -42,10 +45,18 @@ public class RetryButton : MonoBehaviour
         ui.SetActive(false);
     }
 
-    // 난이도 선택 및 씬 이동
-    public void SelectDifficulty(string difficulty)
+
+    // 레벨 1 버튼
+    public void OnLevel1Button()
     {
-        // 난이도 설정 및 게임 시작
-        SceneManager.LoadScene("MainScene");
+        gameSpeed = 1.0f; // 기본 속도
+        SceneManager.LoadScene("MainScene"); // 메인 씬 로드
+    }
+
+    // 레벨 2 버튼
+    public void OnLevel2Button()
+    {
+        gameSpeed = 2.0f; // 2배 속도
+        SceneManager.LoadScene("MainScene"); // 메인 씬 로드
     }
 }
